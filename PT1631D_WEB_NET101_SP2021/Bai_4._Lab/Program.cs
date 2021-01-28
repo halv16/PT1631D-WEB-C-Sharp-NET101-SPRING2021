3875ecb3862e173a608cddcc4f7356677a4ba30a
halv16 <halv16@fe.edu.vn>
2021-01-28 19:44:14 +0700

    update code lab4

---
+++ PT1631D_WEB_NET101_SP2021/Bai_4._Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_4._Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Student student2 = new Student();
+
+            Console.WriteLine("Nhap thong tin sinh vien 1:");
+            student1.InputStudent();
+            Console.WriteLine("In ra thong tin sinh vien 1:");
+            student1.OutputStudent();
+
+            Console.WriteLine("Xep loai " + student1.ValuateStudent(6));
+
+            Console.WriteLine("Nhap thong tin sinh vien 2:");
+            student2.InputStudent();
+            Console.WriteLine("In ra thong tin sinh vien 2:");
+            student2.OutputStudent();
+
+            Console.WriteLine("Xep loai " + student2.ValuateStudent(6));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PT1631D_WEB_NET101_SP2021/Bai_4._Lab/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_4._Lab
+{
+    public class Student
+    {
+        private string name;
+        private string yearofBirth;
+        private string studentId;
+        private double gpa;
+
+        public void OutputStudent()
+        {
+            Console.WriteLine($"{name} - {yearofBirth} - {studentId} - {gpa}");
+        }
+
+        public void InputStudent()
+        {
+            Console.Write("Nhap Ten: ");
+            name = Console.ReadLine();
+
+            Console.Write("Nhap Nam Sinh: ");
+            yearofBirth = Console.ReadLine();
+
+            Console.Write("Nhap Id: ");
+            studentId = Console.ReadLine();
+
+            Console.Write("Nhap GPA: ");
+            gpa = double.Parse(Console.ReadLine());
+        }
+
+        public string ValuateStudent(double gpa)
+        {
+            string valuate;
+
+            if (gpa >= 8)
+            {
+                valuate = "Xuat Sac";
+            }
+            else if (gpa > 6)
+            {
+                valuate = "Gioi";
+            }
+            else if (gpa >= 5)
+            {
+                valuate = "Kha";
+            }
+            else
+            {
+                valuate = "Hoc lai";
+            }
+
+            return valuate;
+        }
+    }
+}
