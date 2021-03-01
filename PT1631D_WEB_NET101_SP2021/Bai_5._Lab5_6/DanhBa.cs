e63beee5938750c9721386573f397dc6bb2078e0
halv16 <halv16@fe.edu.vn>
2021-03-01 14:56:36 +0700

    update code lab5_6

---
+++ PT1631D_WEB_NET101_SP2021/Bai_5._Lab5_6/DanhBa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_5._Lab5_6
+{
+    public class DanhBa
+    {
+        public string HoTen { get; set; }
+
+        public string Sdt1 { get; set; }
+
+        public string Sdt2 { get; set; }
+
+        public int NamSinh { get; set; }
+
+        public string GioiTinh { get; set; }
+
+        public DanhBa()
+        {
+        }
+
+        public void InThongTin()
+        {
+            int year = 2000;
+
+            string xungHo = "Em";
+            if (NamSinh <= year)
+            {
+                if (GioiTinh.ToUpper() == "NAM")
+                {
+                    xungHo = "Anh";
+                }
+                else
+                {
+                    xungHo = "Chị";
+                }
+            }
+
+            Console.WriteLine("{0} {1} \t| {2} \t| {3} \t| {4} \t| {5}", xungHo, HoTen, NamSinh, GioiTinh, Sdt1, Sdt2);
+        }
+    }
+}
---
+++ PT1631D_WEB_NET101_SP2021/Bai_5._Lab5_6/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bai_5._Lab5_6
+{
+    class Program
+    {
+        private static List<DanhBa> list = new List<DanhBa>();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine(" ________________________________");
+                Console.WriteLine("|__________MENU__________________|");
+                Console.WriteLine("| 1. Thêm danh bạ                |");
+                Console.WriteLine("| 2. In mảng                     |");
+                Console.WriteLine("| 3. Tìm theo số điện thoại      |");
+                Console.WriteLine("| 0. Thoát                       |");
+                Console.WriteLine("|________________________________|");
+
+                Console.Write("Mời bạn chọn menu: ");
+                int menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine("| 1. Thêm danh bạ                |");
+                        ThemDanhBa(list);
+                        InMang(list);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("| 2. In mảng                     |");
+                        InMang(list);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("| 3. Tìm theo số điện thoại      |");
+                        TimTheoSdt();
+                        break;
+
+                    default:
+                        condition = false;
+                        Console.WriteLine("Tạm biệt và hẹn gặp lại!!!");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ThemDanhBa(List<DanhBa> list)
+        {
+            Console.WriteLine("Nhập thông tin: ");
+            DanhBa danhBa = new DanhBa();
+            Console.Write("Tên: ");
+            danhBa.HoTen = Console.ReadLine();
+
+            Console.Write("Năm sinh: ");
+            danhBa.NamSinh = int.Parse(Console.ReadLine());
+
+            Console.Write("Giới tính: ");
+            danhBa.GioiTinh = Console.ReadLine();
+
+            Console.Write("Sdt1: ");
+            danhBa.Sdt1 = Console.ReadLine();
+
+            Console.Write("Sdt2: ");
+            danhBa.Sdt2 = Console.ReadLine();
+
+            list.Add(danhBa);
+        }
+
+        private static void InMang(List<DanhBa> lst)
+        {
+            Console.WriteLine("Danh sách trong danh bạ: ");
+            for (int i = 0; i < lst.Count; i++)
+            {
+                Console.Write("{0}.\t", i + 1);
+                lst[i].InThongTin();
+            }
+        }
+
+        private static void TimTheoSdt()
+        {
+            Console.Write("Nhập số điện thoại: ");
+            string sdt = Console.ReadLine();
+
+            Console.WriteLine("Kết quả tìm kiếm: ");
+            int k = 0;
+            foreach (var item in list)
+            {
+                if (item.Sdt1.Contains(sdt) || item.Sdt2.Contains(sdt))
+                {
+                    k++;
+
+                    item.InThongTin();
+                }
+            }
+
+            if (k == 0)
+                Console.WriteLine("Không tìm thấy!");
+        }
+    }
+}
